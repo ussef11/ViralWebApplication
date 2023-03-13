881b84610d0c43b902d3c95d68b40e13276e39a1
Youssef BELKHIRAOUI <69126473+ussef11@users.noreply.github.com>
2023-03-13 01:39:16 +0100

    Update home.js

--- src/Home/home.js
+++ src/Home/home.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Home = () => {
 
 
-const {Data:blog}  = usefetch("https://viral-web-application-pl3j.vercel.app/getAllArticles")
+const {Data:blog}  = usefetch("https://viral-web-application-pl3j-agyzf8hrl-ussef11.vercel.app/getallarticles")
 
 
     return ( 
@@ -13,7 +13,7 @@ const {Data:blog}  = usefetch("https://viral-web-application-pl3j.vercel.app/get
 
 <div className="band">
   <div className="item-1">
-        <a href={`http://localhost:3000/pages/1`} className="card">
+        <a href={`https://www.koora4lives.live/pages/1`} className="card">
           <div  style={{backgroundImage:`url(  https://img.freepik.com/free-photo/people-taking-part-business-event_23-2149346666.jpg?t=st=1678496126~exp=1678496726~hmac=063925156b10d7307fb1b3b996e7b97c582193b6d2c91b2f543185eba7724fb6)`}} className="thumb"></div>
           <article>
             <h1>The Challenge of Formulating Business Intelligence Strategy  </h1>
@@ -26,7 +26,7 @@ const {Data:blog}  = usefetch("https://viral-web-application-pl3j.vercel.app/get
 { 
  blog && blog.map((art)=>(
 <div className="item-2">
-        <a href={`http://localhost:3000/pages/${art.id_articles}`} className="card">
+        <a href={`https://www.koora4lives.live/pages/${art.id_articles}`} className="card">
           <div style={{backgroundImage:`url(${art.articleimg})`}} className="thumb"></div>
           <article>
             <h1>{art.title}</h1>
